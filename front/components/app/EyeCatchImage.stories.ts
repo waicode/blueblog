b4f91bf56958b3c9c6470d077eda63b351f6ff5c
waicode <waicode37@gmail.com>
2022-06-11 01:19:23 +0900

    feat: add EyeCatchImage

--- front/components/app/EyeCatchImage.stories.ts
+++ front/components/app/EyeCatchImage.stories.ts
@@ -1,1 +1,35 @@
+import AppEyeCatchImage from "./EyeCatchImage.vue";
 
+export default {
+  title: "@component/app/EyeCatchImage",
+  component: AppEyeCatchImage,
+  args: {
+    icon: "tabler:calendar-event",
+    width: 884,
+    height: 464,
+  },
+  argTypes: {
+    icon: {
+      control: { type: "text" },
+    },
+    width: {
+      control: { type: "number" },
+    },
+    height: {
+      control: { type: "number" },
+    },
+  },
+};
+
+const Template = (args) => ({
+  components: { AppEyeCatchImage },
+  setup() {
+    return { args };
+  },
+  template: `
+    <AppEyeCatchImage v-bind="args" />
+  `,
+});
+
+export const Primary = Template.bind({});
+Primary.args = {};
--- front/components/app/EyeCatchImage.vue
+++ front/components/app/EyeCatchImage.vue
@@ -1,1 +1,74 @@
+<script setup lang="ts">
+import { Icon } from "@iconify/vue";
 
+import useId from "@/composables/useId";
+import useCss from "@/composables/useCss";
+import { bemx } from "@/utils/util";
+
+interface EyeCatchImagePropType {
+  /**
+   * アイコン指定文字列
+   *
+   * iconifyに指定する文字列
+   */
+  icon: string;
+
+  /**
+   * 幅
+   *
+   * アイキャッチ画像の幅
+   */
+  width: number;
+
+  /**
+   * 高さ
+   *
+   * アイキャッチ画像の高さ
+   */
+  height: number;
+}
+
+const props = defineProps<EyeCatchImagePropType>();
+
+const id = useId();
+const className = bemx("AppEyeCatchImage", id);
+useCss(
+  () =>
+    `
+    .AppEyeCatchImage--${id} .AppEyeCatchImage__Bg {
+      width: ${props.width}px !important;
+      height: ${props.height}px !important;
+    }
+    .AppEyeCatchImage--${id} .AppEyeCatchImage__Icon svg {
+      width: ${Math.trunc(props.width / 1.5)}px !important;
+      height: ${Math.trunc(props.height / 1.5)}px !important;
+    }
+    `
+);
+</script>
+
+<template>
+  <div :class="className">
+    <div class="AppEyeCatchImage__Bg">
+      <div class="AppEyeCatchImage__Icon">
+        <Icon :icon="icon" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+.AppEyeCatchImage {
+  &__Bg {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background: linear-gradient(220.55deg, #7cf7ff 0%, #4b73ff 100%);
+  }
+  &__Icon {
+    svg {
+      color: #fff;
+    }
+  }
+}
+</style>
