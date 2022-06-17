1103ab3e89e42ca73e3175f7e0fb258e4c6e3ded
waicode <waicode37@gmail.com>
2022-06-18 08:38:42 +0900

    chore: prettier setting

---
+++ front/.prettierrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: true,
+  overrides: [
+    {
+      files: '{,.**/,**/}{.*,*}.json',
+      options: {
+        printWidth: 80,
+      },
+    },
+  ],
+}
