e7cdcbd62c2cc1b237d4d8bad7d0f3bd130ad4e6
waicode <waicode37@gmail.com>
2022-07-11 23:48:10 +0900

    feat: add AppShareButtons

---
+++ front/components/app/ShareButtons.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="AppShareButtons">
+    <div class="AppShareButtons__ShareText">SHARE</div>
+    <div class="AppShareButtons__Twitter"></div>
+    <div class="AppShareButtons__Hatena"></div>
+    <div class="AppShareButtons__Facebook"></div>
+    <div class="AppShareButtons__Pocket"></div>
+  </div>
+</template>
+
+<style lang="scss"></style>
